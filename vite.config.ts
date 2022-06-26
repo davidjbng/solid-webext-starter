5f00e36d923c9c12cd12506e6880f6c0b182e5c4
David Jbng <davidjbng@gmail.com>
2022-06-27 00:08:38 +0200

    init from solid vitest template

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import webExtension from "vite-plugin-web-extension";
+import path from "node:path"
+
+export default defineConfig({
+  plugins: [solidPlugin(), webExtension({
+    manifest: path.resolve(__dirname, "manifest.json"),
+    assets: "assets",
+  }), ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupVitest.ts',
+    // solid needs to be inline to work around
+    // a resolution issue in vitest:
+    deps: {
+      inline: [/solid-js/],
+    },
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    threads: false,
+    isolate: false,
+  },
+  build: {
+    target: 'esnext',
+    polyfillDynamicImport: false,
+  },
+  resolve: {
+    conditions: ['development', 'browser'],
+  }
+});
