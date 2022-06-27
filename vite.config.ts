d15debdb479afd9bf3b1e0aaf27bf2db6b0a26bd
David Jbng <davidjbng@gmail.com>
2022-06-27 23:51:25 +0200

    fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  root: 'src',
   plugins: [solidPlugin()],
   test: {
     environment: 'jsdom',
