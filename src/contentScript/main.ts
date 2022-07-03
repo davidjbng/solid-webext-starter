97ddf23a4ceec0a5e376913f960bec5fbe657b2e
David Jebing <david.jebing@lise.de>
2022-07-04 00:24:12 +0200

    remove sender logging

--- src/contentScript/main.ts
+++ src/contentScript/main.ts
@@ -2,7 +2,6 @@ import { onMessage } from '../messaging';
 
 console.log('Content script loaded');
 
-onMessage('hello', (message, sender) => {
-  console.log('Revieved message from sender: ', sender);
+onMessage('hello', (message) => {
   console.log(`Hello, ${message.data.name}`);
 });
